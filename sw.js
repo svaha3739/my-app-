d2c5563212ff0a800fed817261d6c03f53063134
svaha3739 <svaha3739@gmail.com>
2026-07-17 13:16:52 +0900

    디자인 앱 수정 2

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-// 버전 이름을 v2로 올려서 스마트폰이 강제로 새로고침하게 만듭니다.
-const CACHE_NAME = 'slow-jogging-v2';
+// 캐시 이름을 버전 삼으로 올려서 옛날 기억을 지웁니다.
+const CACHE_NAME = 'slow-jogging-v3';
 const urlsToCache = [
   './',
   './index.html',
-  './main.js',
-  './style.css',
-  './icon.jpeg'
+  './main.js?v=3',
+  './style.css?v=3',
+  './Icon.jpeg'
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting(); // 즉시 설치 명령
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
@@ -24,7 +24,7 @@ self.addEventListener('activate', event => {
       return Promise.all(
         cacheNames.map(cache => {
           if (cache !== CACHE_NAME) {
-            return caches.delete(cache); // 이전 버전 쓰레기통 비우기
+            return caches.delete(cache);
           }
         })
       );
