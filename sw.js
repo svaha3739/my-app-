f673261816429b1f43314d8c337233048959e3a7
svaha3739 <svaha3739@gmail.com>
2026-07-18 00:54:49 +0900

    앱 설치중7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jogging-app-v1';
+const CACHE_NAME = 'jogging-app-final-v1';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -8,7 +8,6 @@ self.addEventListener('activate', event => {
   clients.claim();
 });
 
-// 스마트폰이 앱으로 인식하기 위한 필수 조건 (가짜로라도 응답해주는 기능)
 self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request).catch(() => {
